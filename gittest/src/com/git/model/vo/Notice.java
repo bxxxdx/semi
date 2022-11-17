37f776f1e7e01f821be285c435e81219f417f873
GDJ56 <GDJ56@DESKTOP-MRG810C>
2022-11-17 11:17:47 +0900

    bs notice vo 생성중...

---
+++ gittest/src/com/git/model/vo/Notice.java
@@ -0,0 +1,13 @@
+package com.git.model.vo;
+
+public class Notice {
+	private String noticeTitle;
+	private String noticeContent;
+	private String noticeWriter;
+	
+	public Notice() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
